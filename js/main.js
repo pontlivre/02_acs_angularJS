d9580c0c5f17cf49d93a8196e244d70bc0fd87f2
hashimoto <mpontlivre@gmail.com>
2014-10-21 00:32:39 +0900

    トップページ、第１回、Powerd byを作成。

--- js/main.js
+++ js/main.js
@@ -6,26 +6,63 @@
                 gfm: true,
                 tables: true
                 /*
-                sanitize: true,
                 highlight: function (code) {
                     return hljs.highlightAuto(code).value;
-                  }
-                  */
+                }
+                */
             });
     }])
     .config(['$routeProvider', function($routeProvider){
         $routeProvider
+            .when('/00', {
+                controller : 'topCtrl',
+                templateUrl: 'partials/top.html'
+            })
             .when('/01', {
-                controller : 'PageCtrl',
+                controller : 'pageCtrl',
                 templateUrl: 'partials/sample01.html'
-            });
+            })
+            .when('/02', {
+                controller : 'pageCtrl',
+                templateUrl: 'partials/sample02.html'
+            })
+            .when('/powerdBy', {
+                controller : 'pageCtrl',
+                templateUrl: 'partials/powerd_by.html'
+            })
+            /*
+            .otherWise({
+                controller : 'topCtrl',
+                templateUrl: 'partials/top.html'
+            })
+*/
+            ;
     }])
-    .controller('PageCtrl', ['$scope', function($scope){
-
+    .controller('mainCtrl', ['$location', '$scope', function($location, $scope){
+        $scope.pageInfo = { 'no' : 'top' };
+        $scope.active = function(num) {
+            if (('/' + num) == $location.path()) {
+                return 'active';
+            }
+            return '';
+        }
+        $scope.pageNo = $location.path();
+        $scope.$on('$routeChangeSuccess', function(event, data){
+            var path = $location.path();
+            if (path) {
+                $scope.pageInfo.no = path.substr(1);
+            } else {
+                $scope.pageInfo.no = 'top';
+            }
+            console.info('change=' + $location.path());
+            console.info(data);
+        });
+    }])
+    .controller('pageCtrl', ['$scope', function($scope){
+    }])
+    .controller('topCtrl', ['$scope', function($scope){
     }])
     .run(function() {
-        console.info('RUN')	;
-        // hljs.initHighlightingOnLoad();
     });
 
 })(angular);
