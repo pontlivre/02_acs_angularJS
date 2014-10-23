73fd5ee8bc2aa44296f4734b1573cdddf7209cdd
hashimoto <mpontlivre@gmail.com>
2014-10-24 07:42:08 +0900

    add sample02 templates.

---
+++ js/partials/sample02.js
@@ -0,0 +1,8 @@
+var Sample02Ctrl = function($scope) {
+	$scope.books = [
+		{ "title" : "Java入門"},
+		{ "title" : "JavaScript入門"},
+		{ "title" : "Ruby入門"}
+	];
+	console.info('Sample02Ctrl created');
+}
